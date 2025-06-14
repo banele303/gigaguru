e3fb7119f7cd571ccc065a95c0c335accc23c4fa
alex banele <alexsouthflow@gmail.com>
2025-06-14 17:39:18 +0200

    bebe

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -10,11 +10,11 @@ export default function StoreFrontLayout({
   children: ReactNode;
 }) {
   return (
-    <>
+    <div className="bg-white">
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
       <Toaster />
       <Footer />
-    </>
+    </div>
   );
 }
