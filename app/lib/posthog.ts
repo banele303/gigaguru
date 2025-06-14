7f4887789e70b44d31ef53f721d396400841af80
alex banele <alexsouthflow@gmail.com>
2025-06-14 16:29:54 +0200

    nnnn

--- app/lib/posthog.ts
+++ app/lib/posthog.ts
@@ -4,9 +4,6 @@ if (typeof window !== "undefined") {
   try {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-      loaded: (posthog) => {
-        if (process.env.NODE_ENV === "development") posthog.debug();
-      },
       capture_pageview: true,
       capture_pageleave: true,
       autocapture: true,
